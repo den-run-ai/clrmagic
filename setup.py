3a960fb2f1cda3ab84620b145137bd604fa1a57e
denfromufa <denfromufa@users.noreply.github.com>
2016-09-11 22:29:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,17 +29,17 @@ setup(
     pymodules = ["clrmagic"],
     install_requires = ["pythonnet"],
     classifiers = [
-        Development Status :: 3 - Alpha
-        Environment :: Web Environment
-        Framework :: IPython
-        Intended Audience :: Science/Research
-        License :: OSI Approved :: MIT License
-        Operating System :: Microsoft
-        Programming Language :: C#
-        Programming Language :: Python
-        Programming Language :: Python :: Implementation :: CPython
-        Topic :: Scientific/Engineering
-        Topic :: Software Development
+        "Development Status :: 3 - Alpha"
+        "Environment :: Web Environment",
+        "Framework :: IPython",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft",
+        "Programming Language :: C#",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development"
     ],
     zip_safe = False,
     cmdclass = {
