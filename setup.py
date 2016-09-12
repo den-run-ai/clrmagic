0e7db1f5759b32be604f96dee663b7c993a6a185
denfromufa <denfromufa@users.noreply.github.com>
2016-09-11 22:47:53 -0500

    Update setup.py
    forgot brackets

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
     zip_safe = False,
     data_files = [
         ("{install_platlib}", ["{build_lib}/clrmagic.dll"]),
+    ],
     cmdclass = {
         "build_ext": clrmagic_build_ext
     }
