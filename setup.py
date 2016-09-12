3b873c12cfe1fcefc1bd72d6f108c302cd9b187d
denfromufa <denfromufa@users.noreply.github.com>
2016-09-11 23:07:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 from distutils.command.build_ext import build_ext
+from distutils.command.install_data import install_data
 import sys
 
 full_info = open("README.md").read()
@@ -16,6 +17,24 @@ class clrmagic_build_ext(build_ext):
             "/target:library"
         ]
         check_call(" ".join(cmd), shell=True)    
+
+class clrmagic_install_data(install_data):
+    def run(self):
+        build_cmd = self.get_finalized_command("build_ext")
+        install_cmd = self.get_finalized_command("install")
+        build_lib = os.path.abspath(build_cmd.build_lib)
+        install_platlib = os.path.relpath(install_cmd.install_platlib, self.install_dir)
+
+        for i, data_files in enumerate(self.data_files):
+            if isinstance(data_files, str):
+                self.data_files[i] = data_files[i].format(build_lib=build_lib)
+            else:
+                for j, filename in enumerate(data_files[1]):
+                    data_files[1][j] = filename.format(build_lib=build_lib)
+                dest = data_files[0].format(install_platlib=install_platlib)
+                self.data_files[i] = dest, data_files[1]
+
+        return install_data.run(self)
 
 setup(
     name = "clrmagic",
@@ -46,7 +65,8 @@ setup(
         ("{install_platlib}", ["{build_lib}/clrmagic.dll"])
     ],
     cmdclass = {
-        "build_ext": clrmagic_build_ext
+        "build_ext": clrmagic_build_ext,
+        "install_data": clrmagic_install_data
     }
 )
     
